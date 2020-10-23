cd97ee40d85fb7a7f5a547497a456e24f38a7c4a
@Art <69041943+cyberArtik@users.noreply.github.com>
2020-10-23 16:32:06 +0300

    Add files via upload

---
+++ spaceX/js/main.js
@@ -0,0 +1,36 @@
+var instance = new vidbg(".video", {
+    mp4: 'video/world.mp4', // URL or relative path to MP4 video
+    webm: 'video/world.webm', // URL or relative path to webm video
+    poster: 'video/poster.jpg', // URL or relative path to fallback image
+    overlay: false, // Boolean to display the overlay or not
+   });
+var rellax = new Rellax('.rocket');
+if (document.body.clientWidth < 675) {
+    rellax.destroy();
+}
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 3000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
